e24e9f0621467f6f8dd5e28d1aef6868107000f8
hadeedtariq <computeranalog351@gmail.com>
2024-08-10 03:35:09 -0700

    define actions for drawing

---
+++ src/utils/constants.ts
@@ -0,0 +1,13 @@
+type Actions = {
+  SELECT: string;
+  RECTANGLE: string;
+  CIRCLE: string;
+  SCRIBBLE: string;
+};
+
+export const DrawingActions: Actions = {
+  SELECT: "SELECT",
+  RECTANGLE: "RECTANGLE",
+  CIRCLE: "CIRCLE",
+  SCRIBBLE: "SCRIBBLE",
+};
