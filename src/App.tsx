c9eb8cc618da8c5e3a58604fae7deafc975f958c
hadeedtariq <computeranalog351@gmail.com>
2024-08-12 03:26:07 -0700

    create shape creation functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,161 @@
-import Konva from "konva";
-import { useEffect, useState } from "react";
-import { Stage, Layer, Image } from "react-konva";
+import { useRef, useState } from "react";
 import { DrawingActions } from "./utils/constants";
-import { GiArrowCursor } from "react-icons/gi";
 import { TbRectangle } from "react-icons/tb";
-import { FaLongArrowAltRight, FaRegCircle } from "react-icons/fa";
-import { GoPencil } from "react-icons/go";
+import { Circle, Layer, Rect, Stage } from "react-konva";
+import { v4 as uuid } from "uuid";
+import { FaRegCircle } from "react-icons/fa";
 
-export default function App() {
-  const [image, setImage] = useState(new window.Image());
+const App = () => {
   const [action, setAction] = useState(DrawingActions.SELECT);
-  const onDragStart = (e: Konva.KonvaEventObject<DragEvent>) => {
-    e.target.setAttrs({
-      shadowOffSet: {
-        x: 15,
-        y: 15,
-      },
-      scaleX: 1.1,
-      scaleY: 1.1,
-    });
-  };
-  const onDragEnd = (e: Konva.KonvaEventObject<DragEvent>) => {
-    e.target.to({
-      duration: 0.5,
-      easing: Konva.Easings.ElasticEaseOut,
-      scaleX: 1,
-      scaleY: 1,
-      shadowOffsetX: 5,
-      shadowOffsetY: 5,
-    });
+  const [rectangles, setRectAngles] = useState<any>([]);
+  const [circles, setCircles] = useState<any>([]);
+  const strokeColor = "#000";
+  const stageRef = useRef<any>();
+  const isPainting = useRef<any>();
+  const currentShape = useRef<any>();
+  const onPointerDown = () => {
+    if (action === DrawingActions.SELECT) return;
+    const stage = stageRef.current;
+    const { x, y } = stage.getPointerPosition();
+    const id = uuid();
+    currentShape.current = id;
+    isPainting.current = true;
+    if (action === DrawingActions.RECTANGLE) {
+      setRectAngles([
+        ...rectangles,
+        {
+          id,
+          x,
+          y,
+          height: 20,
+          width: 20,
+          fillColor: "#ff0000",
+        },
+      ]);
+    } else {
+      setCircles([
+        ...circles,
+        {
+          id,
+          x,
+          y,
+          radius: 20,
+          fillColor: "#ff0000",
+        },
+      ]);
+    }
   };
 
-  useEffect(() => {
-    const img = new window.Image();
-    img.src =
-      "https://images.unsplash.com/photo-1531804055935-76f44d7c3621?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80";
-    setImage(img);
-  }, []);
-
+  const onPointerMove = () => {
+    if (!isPainting.current || action === DrawingActions.SELECT) return;
+    const stage = stageRef.current;
+    const { x, y } = stage.getPointerPosition();
+    if (action === DrawingActions.RECTANGLE) {
+      setRectAngles((rectangles: any) =>
+        rectangles.map((rectangle: any) => {
+          if (rectangle.id === currentShape.current) {
+            return {
+              ...rectangle,
+              width: x - rectangle.x,
+              height: y - rectangle.y,
+            };
+          }
+          return rectangle;
+        })
+      );
+    } else {
+      setCircles((circles: any) =>
+        circles.map((circle: any) => {
+          if (circle.id === currentShape.current) {
+            return {
+              ...circle,
+              radius: ((y - circle.y) ** 2 + (x - circle.x) ** 2) ** 0.5,
+            };
+          }
+          return circle;
+        })
+      );
+    }
+  };
+  const onPointerUp = () => {
+    isPainting.current = false;
+  };
   return (
-    <div className="relative w-full h-full ">
-      <div className="absolute w-fit h-[50px] border-2 top-0  flex items-center mx-[45%]  p-2 gap-3 z-50">
-        <button
-          className={`cursor-pointer hover:border-2 ${
-            action === DrawingActions.SELECT
-              ? "bg-violet-300 p-1 rounded-md"
-              : "p-1 bg-violet-100"
-          }`}
-          onClick={() => {
-            setAction(DrawingActions.SELECT);
-          }}
+    <>
+      <div className="relative w-full h-screen overflow-hidden">
+        {/* Controls */}
+        <div className="absolute top-0 z-10 w-full py-2 ">
+          <div className="flex justify-center items-center gap-3 py-2 px-3 w-fit mx-auto border shadow-lg rounded-lg">
+            <button
+              className={
+                action === DrawingActions.RECTANGLE
+                  ? "bg-violet-300 p-1 rounded"
+                  : "p-1 hover:bg-violet-100 rounded"
+              }
+              onClick={() => setAction(DrawingActions.RECTANGLE)}
+            >
+              <TbRectangle size={"2rem"} />
+            </button>
+            <button
+              className={
+                action === DrawingActions.CIRCLE
+                  ? "bg-violet-300 p-1 rounded"
+                  : "p-1 hover:bg-violet-100 rounded"
+              }
+              onClick={() => setAction(DrawingActions.CIRCLE)}
+            >
+              <FaRegCircle size={"1.5rem"} />
+            </button>
+          </div>
+        </div>
+        <Stage
+          ref={stageRef as any}
+          height={window.innerHeight}
+          width={window.innerWidth}
+          onPointerDown={onPointerDown}
+          onPointerMove={onPointerMove}
+          onPointerUp={onPointerUp}
         >
-          <GiArrowCursor size={25} className="cursor-pointer" />
-        </button>
-        <button
-          className={`cursor-pointer hover:border-2 ${
-            action === DrawingActions.RECTANGLE
-              ? "bg-violet-300 p-1 rounded-md"
-              : "p-1 bg-violet-100"
-          }`}
-          onClick={() => {
-            setAction(DrawingActions.RECTANGLE);
-          }}
-        >
-          <TbRectangle size={25} className="cursor-pointer" />
-        </button>
-        <button
-          className={`cursor-pointer hover:border-2 ${
-            action === DrawingActions.CIRCLE
-              ? "bg-violet-300 p-1 rounded-md"
-              : "p-1 bg-violet-100"
-          }`}
-          onClick={() => {
-            setAction(DrawingActions.CIRCLE);
-          }}
-        >
-          <FaRegCircle size={25} className="cursor-pointer" />
-        </button>
-        <button
-          className={`cursor-pointer hover:border-2 ${
-            action === DrawingActions.PENCIL
-              ? "bg-violet-300 p-1 rounded-md"
-              : "p-1 bg-violet-100"
-          }`}
-          onClick={() => {
-            setAction(DrawingActions.PENCIL);
-          }}
-        >
-          <GoPencil size={25} className="cursor-pointer" />
-        </button>
-        <button
-          className={`cursor-pointer hover:border-2 ${
-            action === DrawingActions.ARROW
-              ? "bg-violet-300 p-1 rounded-md"
-              : "p-1 bg-violet-100"
-          }`}
-          onClick={() => {
-            setAction(DrawingActions.ARROW);
-          }}
-        >
-          <FaLongArrowAltRight size={25} className="cursor-pointer" />
-        </button>
+          <Layer>
+            {/* <Rect
+              x={0}
+              y={0}
+              height={window.innerHeight}
+              width={window.innerWidth}
+              fill="#ffffff"
+              id="bg"
+            /> */}
+
+            {rectangles?.map((rectangle: any) => (
+              <Rect
+                key={rectangle.id}
+                x={rectangle.x}
+                y={rectangle.y}
+                stroke={strokeColor}
+                strokeWidth={2}
+                fill={rectangle.fillColor}
+                height={rectangle.height}
+                width={rectangle.width}
+                // draggable={isDraggable}
+                // onClick={onClick}
+              />
+            ))}
+            {circles.map((circle: any) => (
+              <Circle
+                key={circle.id}
+                x={circle.x}
+                y={circle.y}
+                radius={circle.radius}
+                fill={circle.fillColor}
+                stroke={strokeColor}
+                strokeWidth={2}
+              />
+            ))}
+          </Layer>
+        </Stage>
       </div>
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <Image
-            x={50}
-            y={50}
-            width={200}
-            height={200}
-            image={image}
-            draggable
-            onDragStart={onDragStart}
-            onDragEnd={onDragEnd}
-          />
-        </Layer>
-      </Stage>
-    </div>
+    </>
   );
-}
+};
+
+export default App;
